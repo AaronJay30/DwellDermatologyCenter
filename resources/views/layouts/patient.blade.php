83aa8848e556d68b6585a0284ebdc3615afb4dc0
AaronJay30 <aaronjaygabato30@gmail.com>
2026-01-26 11:36:35 +0800

    feat: add favicon link to layout files for consistent branding

--- resources/views/layouts/patient.blade.php
+++ resources/views/layouts/patient.blade.php
@@ -8,6 +8,7 @@
     <title>{{ config('app.name', 'Dwell Dermatology Center') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     
